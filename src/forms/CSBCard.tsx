0b192c0621817739131424c48fe094d355ba44ea
Palak <palak@shipglobal.in>
2025-02-05 01:19:58 +0530

    Validations handle using zod superRefine

--- src/forms/CSBCard.tsx
+++ src/forms/CSBCard.tsx
@@ -1,39 +1,40 @@
 import React from "react";
 
 interface CSBCardProps {
-    className?: string;
-    Icon: any;
-    text1: string;
-    text2: string;
-    text3: string;
-    csbNumber: string;
-    iconStyle: string;
-    onSelect: any;
-  }
-  const CSBCard = ({
-    Icon,
-    text1,
-    text2,
-    text3,
-    csbNumber,
-    iconStyle,
-    onSelect,
-  }: CSBCardProps) => {
-    return (
-      <div
-        className="mt-5 p-7 cursor-pointer rounded-md"
-        onClick={onSelect}
-      >
-        <p className="ml-10 font-semibold text-lg">CSB - {csbNumber}</p>
-        <div className="flex items-center gap-14 mt-2">
-          <Icon className={`${iconStyle}`} />
-          <div className="flex flex-col text-sm text-gray-500 font-medium">
-            <p>{text1}</p>
-            <p>{text2}</p>
-            <p>{text3}</p>
-          </div>
+  className?: string;
+  Icon: any;
+  text1: string;
+  text2: string;
+  text3: string;
+  csbNumber: string;
+  iconStyle: string;
+  onSelect: any;
+}
+const CSBCard = ({
+  Icon,
+  text1,
+  text2,
+  text3,
+  csbNumber,
+  iconStyle,
+  onSelect,
+  className,
+}: CSBCardProps) => {
+  return (
+    <div
+      className={`${className} mt-5 p-7 cursor-pointer rounded-md`}
+      onClick={onSelect}
+    >
+      <p className="ml-10 font-semibold text-lg">CSB - {csbNumber}</p>
+      <div className="flex items-center gap-14 mt-2">
+        <Icon className={`${iconStyle}`} />
+        <div className="flex flex-col text-sm text-gray-500 font-medium">
+          <p>{text1}</p>
+          <p>{text2}</p>
+          <p>{text3}</p>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 export default CSBCard;
--- src/layout/schemas.ts
+++ src/layout/schemas.ts
@@ -1,53 +1,109 @@
 import { z } from "zod";
 
-export const BuyerSchema = z.object({
-  shipping_pickup_address: z.string().min(1, "The Pickup Address is required"),
-  shipping_firstname: z
-    .string()
-    .min(1, "The customer shipping first name is required."),
-  shipping_lastname: z
-    .string()
-    .min(1, "The customer shipping last name is required."),
-  shipping_mobile: z
-    .string()
-    .min(10, "The customer mobile number is required."),
-  shipping_alternate_mobile: z.string().optional(),
-  shipping_email: z.string().email("The email is required."),
-  shipping_country: z
-    .string()
-    .min(1, "The customer shipping country code is required."),
-  shipping_address1: z
-    .string()
-    .min(1, "The customer shipping address 1 is required."),
-  shipping_address2: z.string().optional(),
-  shipping_pincode: z
-    .string()
-    .min(5, "The customer shipping postcode is required."),
-  shipping_city: z.string().min(1, "The customer shipping city is required."),
-  shipping_state: z.string().min(1, "The customer shipping state is required."),
-  isBillingSame: z.boolean(),
-  billing_firstname: z
-    .string()
-    .min(1, "The customer billing first name is required."),
-  billing_lastname: z
-    .string()
-    .min(1, "The customer billing last name is required."),
-  billing_mobile: z
-    .string()
-    .min(1, "The customer billing mobile number is required."),
-  billing_country: z
-    .string()
-    .min(1, "The customer billing country is required."),
-  billing_address1: z
-    .string()
-    .min(1, "The customer billing address 1 is required."),
-  billing_address2: z.string().optional(),
-  billing_pincode: z
-    .string()
-    .min(1, "The customer billing pincode is required."),
-  billing_city: z.string().min(1, "The customer billing city is required."),
-  billing_state: z.string().min(1, "The customer billing state is required."),
-});
+export const BuyerSchema = z
+  .object({
+    shipping_pickup_address: z
+      .string()
+      .min(1, "The Pickup Address is required"),
+    shipping_firstname: z
+      .string()
+      .min(1, "The customer shipping first name is required."),
+    shipping_lastname: z
+      .string()
+      .min(1, "The customer shipping last name is required."),
+    shipping_mobile: z
+      .string()
+      .min(10, "The customer mobile number is required."),
+    shipping_alternate_mobile: z.string().optional(),
+    shipping_email: z.string().email("The email is required."),
+    shipping_country: z
+      .string()
+      .min(1, "The customer shipping country code is required."),
+    shipping_address1: z
+      .string()
+      .min(1, "The customer shipping address 1 is required."),
+    shipping_address2: z.string().optional(),
+    shipping_pincode: z
+      .string()
+      .min(5, "The customer shipping postcode is required."),
+    shipping_city: z.string().min(1, "The customer shipping city is required."),
+    shipping_state: z
+      .string()
+      .min(1, "The customer shipping state is required."),
+
+    isBillingSame: z.boolean(),
+
+    // Billing fields (Initially optional)
+    billing_firstname: z.string().optional(),
+    billing_lastname: z.string().optional(),
+    billing_mobile: z.string().optional(),
+    billing_country: z.string().optional(),
+    billing_address1: z.string().optional(),
+    billing_address2: z.string().optional(),
+    billing_pincode: z.string().optional(),
+    billing_city: z.string().optional(),
+    billing_state: z.string().optional(),
+  })
+  .superRefine((data, ctx) => {
+    if (!data.isBillingSame) {
+      if (!data.billing_firstname) {
+        ctx.addIssue({
+          path: ["billing_firstname"],
+          message: "The customer billing first name is required.",
+          code: "custom",
+        });
+      }
+      if (!data.billing_lastname) {
+        ctx.addIssue({
+          path: ["billing_lastname"],
+          message: "The customer billing last name is required.",
+          code: "custom",
+        });
+      }
+      if (!data.billing_mobile) {
+        ctx.addIssue({
+          path: ["billing_mobile"],
+          message: "The customer billing mobile number is required.",
+          code: "custom",
+        });
+      }
+      if (!data.billing_country) {
+        ctx.addIssue({
+          path: ["billing_country"],
+          message: "The customer billing country is required.",
+          code: "custom",
+        });
+      }
+      if (!data.billing_address1) {
+        ctx.addIssue({
+          path: ["billing_address1"],
+          message: "The customer billing address 1 is required.",
+          code: "custom",
+        });
+      }
+      if (!data.billing_pincode) {
+        ctx.addIssue({
+          path: ["billing_pincode"],
+          message: "The customer billing pincode is required.",
+          code: "custom",
+        });
+      }
+      if (!data.billing_city) {
+        ctx.addIssue({
+          path: ["billing_city"],
+          message: "The customer billing city is required.",
+          code: "custom",
+        });
+      }
+      if (!data.billing_state) {
+        ctx.addIssue({
+          path: ["billing_state"],
+          message: "The customer billing state is required.",
+          code: "custom",
+        });
+      }
+    }
+  });
 
 export const OrderSchema = z.object({
   id: z.string().min(2),
